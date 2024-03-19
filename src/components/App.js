eb416fdb9000e87a3626f878269dff653e7be987
jakes desktop <jperez32017@gmail.com>
2024-03-18 21:31:35 -0700

    added useReducer and more components

---
+++ src/components/App.js
@@ -0,0 +1,75 @@
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import { useEffect, useReducer } from "react";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
+
+// state objects
+const initialState = {
+  questions: [],
+  // 'loading', 'error', 'ready', 'active', 'finished
+  status: "Loading",
+  index: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return {
+        ...state,
+        questions: action.payload,
+        status: "ready",
+      };
+    case "dataFailed":
+      return {
+        ...state,
+        status: "error",
+      };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+      };
+    default:
+      throw new Error("Action unknown");
+  }
+}
+
+export default function App() {
+  // creates initial state object and reducer
+  // destructure state
+  // const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  //Num of questions
+  const numQuestions = questions.length;
+  //load data on mount
+
+  useEffect(function () {
+    // if type == dataReceived, the payload will be the json data. aka action
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataReceived", payload: data }))
+      .catch((err) => dispatch({ type: "dataFailed" }));
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
+      </Main>
+    </div>
+  );
+}
